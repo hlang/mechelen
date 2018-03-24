b8ed8e72282b2cb67058208089553c70a847cc84
Angular CLI <angular-cli@angular.io>
2018-03-24 17:14:16 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ src/main/java/de/hartmut/lang/mechelen/MechelenApplication.java
@@ -0,0 +1,12 @@
+package de.hartmut.lang.mechelen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechelenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechelenApplication.class, args);
+	}
+}
